8a1155a291863ece98629e63dd81eeac02fd33a9
Hassan Khan <162082684+HK-Adventures@users.noreply.github.com>
2024-10-02 14:53:25 +0500

    Add files via upload

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,15 @@
+import Header from "@/app/components/header";
+import Footer from "@/app/components/footer";
+export default function contact () {
+    return (
+        <div className="headerfirst">
+            <Header />
+            <div className="contactinfo">
+                Phone Numer: +92 319 1939538
+            </div>
+            <div className="footer">
+          <Footer />
+        </div>
+        </div>
+    );
+}
